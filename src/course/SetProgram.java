16b210ed55d237a5278d994ff582258bac3ac571
sergio-macedo <sergio.garoma@hotmail.com>
2022-06-13 12:48:08 +0200

    set all, remove and retain

--- src/course/SetProgram.java
+++ src/course/SetProgram.java
@@ -1,26 +1,25 @@
 package course;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SetProgram {
     public static void main(String[] args) {
 
-        //  Set<String> set = new HashSet<>(); //if the order does not matter hashset is your guy :-)
-        // Set<String> set = new TreeSet<>(); // the treeSet comes with the order.
-        Set<String> set = new LinkedHashSet<>(); // it comes in the order that is was putted. Egal was.
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 
-        set.add("Tv");
-        set.add("Tablet");
-        set.add("Notebook");
-
-        set.removeIf((x -> x.charAt(1) == 'o'));
-
-        for (String p : set) {
-            System.out.println(p);
-        }
+        //union
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
+        //Intersection
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println(d);
+        //difference
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println(e);
 
     }
 }
